8e514745c171453568f48778e135fb494765da4b
alex <qwert2603@mail.ru>
2019-02-19 20:02:22 +0700

    cancel message on message with same command

--- app/src/main/java/com/qwert2603/btablo/model/SettingsRepo.kt
+++ app/src/main/java/com/qwert2603/btablo/model/SettingsRepo.kt
@@ -44,31 +44,31 @@ class SettingsRepo(private val tabloInterface: TabloInterface) {
 //            renderIfChangedEqual({ team2 }) { tabloInterface.setTeam2Name(it).makeSend() }
 
             if (isFirstRendering()) {
-                tabloInterface.setTeam1Name(vs.team1).makeSend()
-                tabloInterface.setTeam2Name(vs.team2).makeSend()
+                tabloInterface.setTeam1Name(vs.team1).makeSend(TabloConst.Command.CMD_MES1)
+                tabloInterface.setTeam2Name(vs.team2).makeSend(TabloConst.Command.CMD_MES2)
             }
 
             renderIfChangedEqual({ minutes to seconds }) { (minutes, seconds) ->
-                tabloInterface.setTime(minutes, seconds).makeSend()
+                tabloInterface.setTime(minutes, seconds).makeSend(TabloConst.Command.CMD_TIME)
             }
 
             renderIfChangedEqual({ points1 to points2 }) { (points1, points2) ->
-                tabloInterface.setScore(points1, points2).makeSend()
+                tabloInterface.setScore(points1, points2).makeSend(TabloConst.Command.CMD_SCORE)
             }
 
-            renderIfChangedEqual({ period }) { tabloInterface.setPeriod(it).makeSend() }
+            renderIfChangedEqual({ period }) { tabloInterface.setPeriod(it).makeSend(TabloConst.Command.CMD_PEROID) }
 
             renderIfChangedEqual({ fouls1 to fouls2 }) { (fouls1, fouls2) ->
-                tabloInterface.setFouls(fouls1, fouls2).makeSend()
+                tabloInterface.setFouls(fouls1, fouls2).makeSend(TabloConst.Command.CMD_FOUL)
             }
 
             renderIfChangedEqual({ timeouts1 to timeouts2 }) { (timeouts1, timeouts2) ->
-                tabloInterface.setTimeouts(timeouts1, timeouts2).makeSend()
+                tabloInterface.setTimeouts(timeouts1, timeouts2).makeSend(TabloConst.Command.CMD_TIMEOUT)
             }
 
-            renderIfChangedEqual({ holdIsTeam2 }) { tabloInterface.setHolding(it).makeSend() }
+            renderIfChangedEqual({ holdIsTeam2 }) { tabloInterface.setHolding(it).makeSend(TabloConst.Command.CMD_HANDLING) }
 
-            renderIfChangedEqual({ attackSeconds }) { tabloInterface.setTimeAttack(it, it == 0).makeSend() }
+            renderIfChangedEqual({ attackSeconds }) { tabloInterface.setTimeAttack(it, it == 0).makeSend(TabloConst.Command.CMD_24SEC) }
         }
     }
 
@@ -78,7 +78,7 @@ class SettingsRepo(private val tabloInterface: TabloInterface) {
     val isStarted: Observable<Boolean> = _isStarted.hide()
     val isAttackStarted: Observable<Boolean> = _isAttackStarted.hide()
 
-    private val messagesToSend = PublishSubject.create<Completable>()
+    private val messagesToSend = PublishSubject.create<Pair<Completable, TabloConst.Command>>()
 
     private val _sendingState = BehaviorSubject.createDefault<SendingState>(SendingState.NotSent)
 
@@ -127,7 +127,7 @@ class SettingsRepo(private val tabloInterface: TabloInterface) {
                 if (vs.attackSeconds == 0) {
                     setAttackStarted(false)
                     if (i > 0) {
-                        tabloInterface.setSignal1(true).makeSend()
+                        tabloInterface.setSignal1(true).makeSend(TabloConst.Command.CMD_SIREN1)
                     }
                 }
             }
@@ -141,12 +141,13 @@ class SettingsRepo(private val tabloInterface: TabloInterface) {
         resetSendingMessages
             .switchMap {
                 messagesToSend
-                    .concatMap { completable ->
+                    .concatMap { (completable, command) ->
                         completable
                             .toSingleDefault<SendingState>(SendingState.Success)
                             .onErrorReturn { SendingState.Error(it) }
                             .toObservable()
                             .startWith(SendingState.Sending)
+                            .takeUntil(messagesToSend.filter { it.second == command })
                     }
                     .takeUntil { it is SendingState.Error }
                     .doFinally { resetSendingMessages.onNext(Unit) }
@@ -164,8 +165,8 @@ class SettingsRepo(private val tabloInterface: TabloInterface) {
 
     fun sendTeams() {
         val tabloViewState = vs.field
-        tabloInterface.setTeam1Name(tabloViewState.team1).makeSend()
-        tabloInterface.setTeam2Name(tabloViewState.team2).makeSend()
+        tabloInterface.setTeam1Name(tabloViewState.team1).makeSend(TabloConst.Command.CMD_MES1)
+        tabloInterface.setTeam2Name(tabloViewState.team2).makeSend(TabloConst.Command.CMD_MES2)
     }
 
     fun setStarted(started: Boolean) {
@@ -177,14 +178,14 @@ class SettingsRepo(private val tabloInterface: TabloInterface) {
     }
 
     fun setSignal1(enable: Boolean) {
-        tabloInterface.setSignal1(enable).makeSend()
+        tabloInterface.setSignal1(enable).makeSend(TabloConst.Command.CMD_SIREN1)
     }
 
     fun setSignal2(enable: Boolean) {
-        tabloInterface.setSignal2(enable).makeSend()
+        tabloInterface.setSignal2(enable).makeSend(TabloConst.Command.CMD_SIREN2)
     }
 
-    private fun Completable.makeSend() {
-        messagesToSend.onNext(this)
+    private fun Completable.makeSend(command: TabloConst.Command) {
+        messagesToSend.onNext(this to command)
     }
 }
